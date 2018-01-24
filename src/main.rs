f1ede5fcaef695d65ba218e0777415fa0449f080
miyamoen <akihito.biblioteca@gmail.com>
2018-01-24 23:52:40 +0900

    Done Step 3: Implement a service

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,17 @@ extern crate tokio_service;
 
 use std::io;
 use std::str;
+use futures::{future, Future};
 use bytes::BytesMut;
 use tokio_io::codec::{Encoder, Decoder};
 use tokio_proto::pipeline::ServerProto;
 use tokio_io::{AsyncRead, AsyncWrite};
 use tokio_io::codec::Framed;
+use tokio_service::Service;
 
 pub struct LineCodec;
 pub struct LineProto;
+pub struct Echo;
 
 impl Decoder for LineCodec {
     type Item = String;
@@ -62,6 +65,24 @@ impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for LineProto {
     type BindTransport = Result<Self::Transport, io::Error>;
     fn bind_transport(&self, io: T) -> Self::BindTransport {
         Ok(io.framed(LineCodec))
+    }
+}
+
+impl Service for Echo {
+    // These types must match the corresponding protocol types:
+    type Request = String;
+    type Response = String;
+
+    // For non-streaming protocols, service errors are always io::Error
+    type Error = io::Error;
+
+    // The future for computing the response; box it for simplicity.
+    type Future = Box<Future<Item = Self::Response, Error =  Self::Error>>;
+
+    // Produce a future for computing a response from a request.
+    fn call(&self, req: Self::Request) -> Self::Future {
+        // In this case, the response is immediate.
+        Box::new(future::ok(req))
     }
 }
 
